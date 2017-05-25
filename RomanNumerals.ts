db87f87995272877d345c70767f829356adc031f
Philip Juhl <philip.juhl@gmail.com>
2017-05-25 15:28:31 +0200

    Added test and full functionalitet for converting roman to decimal

--- RomanNumerals.ts
+++ RomanNumerals.ts
@@ -1,40 +1,94 @@
 ﻿class RomanLetterValue {
     decimalValue: number;
     romanLetter: string;
+    inFrontOf: string;
 }
 
 var romanLetters = [
     {
         decimalvalue: 1,
-        romanLetter: "I"
+        romanLetter: "I",
+        infrontOf: "VX"
     },
     {
         decimalvalue: 5,
-        romanLetter: "V"
+        romanLetter: "V",
+        infrontOf: ""
     },
     {
         decimalvalue: 10,
-        romanLetter: "X"
+        romanLetter: "X",
+        infrontOf: "LC"
     },
     {
         decimalvalue: 50,
-        romanLetter: "L"
+        romanLetter: "L",
+        infrontOf: ""
     },
     {
         decimalvalue: 100,
-        romanLetter: "C"
+        romanLetter: "C",
+        infrontOf: "DM"
     },
     {
         decimalvalue: 500,
-        romanLetter: "D"
+        romanLetter: "D",
+        infrontOf: ""
     },
     {
         decimalvalue: 1000,
-        romanLetter: "M"
+        romanLetter: "M",
+        infrontOf: ""
     }
 ];
 
-function romandToDecimal(romanNumeral: string) {
-    return romanLetters.filter(item => item.romanLetter === romanNumeral)[0].decimalvalue;
-    //return romanLetters.find((x) => x.romanLetter === romanNumeral);
+function romanToDecimal(romanNumeral: string) {
+    var value = 0;
+    var previousValue = 0;
+    if (toManyRepeatedLetter(romanNumeral))
+        throw new Error("Not a valid roman value '" + romanNumeral + "'");
+    for (var index = 0, len = romanNumeral.length; index < len; index++) {
+        var currentValue = getRomanValue(romanNumeral[index]);
+        if (previousValue < currentValue)
+            value += (currentValue-2*previousValue);
+        else
+            value += currentValue;
+        previousValue = currentValue;
+    }
+    return value;
+
+
+    function getRomanValue(romanLetter: string) {
+        return romanLetters.filter(item => item.romanLetter === romanLetter)[0].decimalvalue;
+    }
+
+    function smallerValueRepeated(previeus: string, current: string, index: number) {
+        if(index === 0)
+            return current;
+        if (getRomanValue(previeus[0]) < getRomanValue(current[0])) {
+            if (previeus.length > 1)
+                throw new Error("To many repeats");
+            if (!letterCanBeInfrontOf(previeus[0], current[0])) {
+                throw new Error("Letter '" + previeus[0] + "' cannot be infront of '" + current[0] + "'.");
+            }
+
+        }
+        return current;
+    }
+
+    function letterCanBeInfrontOf(letterInfront: string, letterBehind: string) {
+        return romanLetters.filter((x) => x.romanLetter === letterInfront)[0].infrontOf.indexOf(letterBehind) !== -1;
+    }
+
+    function toManyRepeatedLetter(romanNumeral: string) {
+        var repeatedLetters = (romanNumeral.toUpperCase().match(/([A-Z])\1*/g));
+
+        if (repeatedLetters && (repeatedLetters.filter((x) => x.length > 3)).length > 0)
+            return true;
+        repeatedLetters.reduce(smallerValueRepeated);
+
+        return false;
+    };
 };
+
+
--- RomanNumeralts_tests.ts
+++ RomanNumeralts_tests.ts
@@ -4,7 +4,66 @@ describe("Roman unittest",
      () => {
          it("Convert single letter",
              () => {
-                 var d = romandToDecimal("I");
+                 var d = romanToDecimal("I");
                  expect(d).toEqual(1);
              });
-     });
+         it("Same letter then add values",
+             () => {
+                 var d = romanToDecimal("II");
+                 expect(d).toEqual(2);
+             });
+         it("Small value before larger value then add values",
+             () => {
+                 var d = romanToDecimal("XI");
+                 expect(d).toEqual(11);
+             });
+         it("Large value before smaller value then subtract values",
+             () => {
+                 var d = romanToDecimal("IX");
+                 expect(d).toEqual(9);
+             });
+         it("Repeat same letter value more than 3 times gives an error",
+             () => {
+                 expect(() => { romanToDecimal("IIII"); }).toThrowError();
+             });
+         it("More than one smaller value infront of greater gives an error",
+             () => {
+                 expect(() => { romanToDecimal("IIV"); }).toThrowError();
+             });
+         it("Letter 'I' can only be infront of 'X' or 'V' otherwice its gives an error",
+             () => {
+                 expect(() => { romanToDecimal("IM"); }).toThrowError();
+             });
+         it("Letter 'X' can only be infront of 'L' or 'C' otherwice its gives an error",
+             () => {
+                 expect(() => { romanToDecimal("XM"); }).toThrowError();
+             });
+         it("Letter 'V' cannot be infront of larger values",
+             () => {
+                 expect(() => { romanToDecimal("VM"); }).toThrowError();
+             });
+         it("Non valid roman letters gives an error",
+             () => {
+                 expect(() => { romanToDecimal("IA"); }).toThrowError();
+             });
+     }
+);
+describe("Roman accepttest",
+    () => {
+        it("Convert roman 'MMCDXLIV' should be 2444",
+            () => {
+                var v = romanToDecimal("MMCDXLIV");
+                expect(v).toEqual(2444);
+            });
+        it("Convert roman 'MCMXCIX' should be 1999",
+            () => {
+                var v = romanToDecimal("MCMXCIX");
+                expect(v).toEqual(1999);
+            });
+        it("Convert roman 'XC' should be 90",
+            () => {
+                var v = romanToDecimal("XC");
+                expect(v).toEqual(90);
+            });
+    }
+);
